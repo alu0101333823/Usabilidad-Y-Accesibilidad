0dd6c2b2b58ecf2b0992d76b4794efd8c1b055b9
alu0101326351 <72520877+alu0101326351@users.noreply.github.com>
2022-04-02 10:09:21 +0100

    Add files via upload

---
+++ Proyecto Grupal/Entrega 4: JavaScript Básico/js/ejercicio3.js
@@ -0,0 +1,7 @@
+var divs = document.getElementsByTagName("div").length;
+var p = document.getElementsByTagName("p").length;
+var li = document.getElementsByTagName("li").length;
+
+alert("Hay " + divs + " etiquetas div");
+alert("Hay " + p + " párrafos");
+alert("Hay " + li + " elementos de lista");
