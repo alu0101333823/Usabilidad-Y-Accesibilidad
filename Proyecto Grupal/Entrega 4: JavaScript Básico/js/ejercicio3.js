b508801b8b4371617bfa68eeb218370b36659638
alu0101333823 <alu0101333823@ull.edu.es>
2022-04-03 13:53:11 +0100

    ejercicios hechos

--- Proyecto Grupal/Entrega 4: JavaScript Básico/js/ejercicio3.js
+++ Proyecto Grupal/Entrega 4: JavaScript Básico/js/ejercicio3.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
   btn.onclick = function () {
     const parrafo = document.createElement('div');
     parrafo.innerHTML = `
-        <h5 class="center">El usuario ha introducido: </h5>
+        <h5 class="center">Elementos de nuestra página: </h5>
         <p class="light">Hay ${divs} bloques "divs"</p>
         <p class="light">Hay ${p} bloques "p"</p>
         <p class="light">Hay ${li} bloques "li"</p>
